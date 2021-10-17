90c1b40bf699b5a942adcd44aa2eb1db6061aea7
Tamara Mijatovic <t.mijatovic@gmail.com>
2021-10-17 14:06:22 +0200

    adapted Population and Landscape classes to work together

--- Landscape.py
+++ Landscape.py
@@ -5,45 +5,67 @@ Created on Wed Oct 13 10:41:46 2021
 @author: tmija
 """
 
-import Population
+from Population import Population
 import numpy as np
 from itertools import product
 
 class Landscape:
-    def __init__(self, connection_mat):
+    def __init__(self, connection_mat, N_copies, N_alleles=2, init_pop=None):
         self.N = len(connection_mat)
-        assert(connection_mat)
-        self.populations = [Population() for n in self.N] # TODO: add arguments
+        assert(len(connection_mat[0])==self.N) # Make sure the matrix is square.
+        self.populations = [Population(N_copies, N_alleles, init_pop) for n in range(self.N)] # TODO: add arguments
         self.connections = connection_mat
         
+        
+    #%% getters and setters
+    
     def get_connections(self):
         return self.connections
     
     def get_genePools(self):
-        return [p.get_genePool() for p in self.populations] # retrieve gene pool of all populations
+        return [p.get_population() for p in self.populations] # retrieve gene pool of all populations
+    
+    def get_normalized_genePools(self):
+        return [p.get_normalized_population() for p in self.populations] # retrieve gene pool of all populations
+    
+    def get_generational_memory(self):
+        return [p.get_generational_memory() for p in self.populations]
+    
+    def set_initial_populations(self, initial_populations):
+        assert(len(initial_populations)==self.N)
+        for n, pop in enumerate(self.populations):
+            pop.create_population(initial_populations[n])
+    
+    
+    #%% exchange and update
     
     def exchange(self):
-        alleles = self.get_genePools(self)
+        alleles = self.get_genePools()
         for i,j in product(range(self.N), range(self.N)):
-            
             p=self.connections[i][j] # exchange from i to j
             if p != 0:
-                exchanged = [allele for allele in alleles[i] if (np.random.rand()<p)] # every allele has a probability of being exchanged
+                exchanged = [np.sum(np.random.rand(allele)<p) for allele in alleles[i]] # every allele has a probability of being exchanged
                 self.populations[j].add_genes(exchanged) # the number of exchanged alleles is random
+                print(exchanged)
                     
-    def next_gen(self):
+    def next_generation(self):
         for n in range(self.N):
-            self.populations[n].next_gen() # calculate the next generation
+            self.populations[n].next_generation() # calculate the next generation
             
     def update(self):
-        self.exchange(self)
-        self.next_gen(self)
+        self.exchange()
+        self.next_generation()
 
+
+#%% Testing
                     
 if __name__=='__main__':
-    connection_mat=[[1,1][1,1]]
-    l = Landscape(connection_mat)
+    connection_mat=[[0,0.3],[0.1,0]]
+    l = Landscape(connection_mat, 100)
+    l.set_initial_populations([[10,1], [3,2]])
+    print(l.get_genePools())
     l.update()
+    print(l.get_generational_memory())
     
                     
         
---
+++ Population.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 20:08:07 2021
+
+@author: leand
+"""
+
+# This is my first attempt at working with github. We will see how this goes!
+
+# I will initially save the populations as small arrays containing
+# The relevant numbers of individuals
+
+import random as rnd
+import numpy as np
+
+class Population:
+    
+    #%% Initialize
+    
+    def __init__(self, N_copies, N_alleles=2, rel_pop=None):
+        # Initializing parameters
+        self.N_copies = N_copies
+        self.N_alleles = N_alleles
+        self.normalized_population = [0 for i in range(N_alleles)]
+        self.generational_memory = []
+        
+        # Creating first generation
+        if rel_pop == None:
+            # As default set all allele frequencies equal
+            rel_pop = [1 for allele in range(N_alleles)]
+        
+        self.create_population(rel_pop)    
+        
+    
+    #%% getters and setters
+    
+    def get_N_copies(self):
+        return self.N_copies
+    
+    def get_N_alleles(self):
+        return self.N_alleles
+    
+    def get_population(self):
+        return [round(self.N_copies*p) for p in self.normalized_population]
+        # round to get rid of floaing point inaccuracies.
+    
+    def get_normalized_population(self):
+        return self.normalized_population
+    
+    def get_generational_memory(self):
+        return self.generational_memory
+    
+    def set_N_copies(self, N_copies):
+        self.N_copies = N_copies
+        
+    def set_N_alleles(self, N_alleles):
+        self.N_alleles = N_alleles
+        self.normalized_population = [1/N_alleles for i in range(N_alleles)]
+        self.generational_memory = []
+        # TODO: make sure population is initialized correctly
+    
+    
+    #%% a couple of helper methods
+    
+    # Calculate the frequency of the alleles in the population.
+    # Changes the instance variable rel_pop.
+    def normalize_population(self, population):
+        return [p/sum(population) for p in population]
+        
+    # helper function because I don't always want to have to call np
+    def cumsum(self, A):
+        return list(np.cumsum(A))
+    
+    # Given a cummulative relative population, draw one random individual from it
+    # Return the result as the index of relative allele in the population.
+    def draw_random_individual_from_relative_population_cum(self, rel_pop_cum):
+        random_number = rnd.random()
+        for i, rpc in enumerate(rel_pop_cum):
+            if random_number < rpc:
+                return i
+        
+    
+    #%% Create a new population
+    
+    # Create a population based on a given relative population.
+    # To ensure that we obtain exactly N_copies individuals:
+    # Top off the rounding errors with draw_random_individual...
+    def create_population(self, rel_pop):
+        # Check if we changed number of alleles. Print a warning
+        if not self.N_alleles == len(rel_pop):
+            print("WARNING: HARD CHANGE IN N_ALLELES")
+            self.N_alleles = len(rel_pop)
+        
+        # Normalize rel_pop
+        rel_pop = self.normalize_population(rel_pop)
+        rel_pop_cum = self.cumsum(rel_pop)
+        
+        # Generate initial guess of new population
+        population = [int(self.N_copies * r) for r in rel_pop]
+        
+        # Fill up until N_copies by randomly pulling from relative population
+        while sum(population) < self.N_copies:
+            population[self.draw_random_individual_from_relative_population_cum(rel_pop_cum)] += 1
+            
+        self.normalized_population = self.normalize_population(population)
+      
+        self.generational_memory = []
+        self.generational_memory.append(self.normalized_population)
+        
+
+    #%% Create the next generation
+    
+    # fetch fixed number of offspring
+    def fetch_offspring(self, N_offspring, rel_pop):
+        rel_pop_cum = self.cumsum(rel_pop)
+        
+        offspring = [0 for i in range(self.N_alleles)]
+        for i in range(N_offspring):
+            offspring[self.draw_random_individual_from_relative_population_cum(rel_pop_cum)] += 1
+        
+        return offspring
+    
+    # spread to neighbors but relative to number of copies
+    def fetch_offspring_relative(self, p_N_copies, rel_pop):
+        return self.fetch_offspring(round(p_N_copies * self.N_copies), rel_pop)
+    
+    # Create a new generation based on a virtual parent generation. New generation will always
+    # have N_copies copies
+    def next_generation(self, prev_pop=None):
+        if prev_pop == None:
+            # by default use current population
+            rel_pop = self.normalized_population
+        else:
+            # if a previous population is given, empty the current generational memory 
+            # and start from the given population
+            self.generational_memory = []
+            rel_pop = self.normalize_population(prev_pop)
+            
+        population = self.fetch_offspring(self.N_copies, rel_pop)
+        
+        self.normalized_population = self.normalize_population(population)
+        self.generational_memory.append(self.normalized_population)
+        
+        
+    #%% Communication with other populations
+    
+    # Allows for the addition of individuals to the population
+    def add_genes(self, added_pop):
+        population = self.get_population()
+        population = [population[i] + added_pop[i] for i in range(self.N_alleles)]
+        self.normalized_population = self.normalize_population(population)
+        
+    
+#%% Testing
+
+if __name__ == '__main__':
+    print("hi!")
+    
+    P = Population(100,2,[1,3])
+    #P.create_population([10, 10, 10])
+    print("Population at start of simulation:", P.get_normalized_population())
+    for i in range(100):
+        P.next_generation()
+    print("Evolution of generations:\n", P.get_generational_memory())
+    
+            
+        
+
