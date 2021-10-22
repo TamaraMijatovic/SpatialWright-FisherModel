f6bcb39258675b54d47bb3e87eff19e1090a532b
LeanderLutze <L.A.Lutze@student.tudelft.nl>
2021-10-22 13:09:14 +0200

    added

---
+++ limitedCaseController.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 13:02:18 2021
+
+@author: leand
+"""
+
---
+++ limitedCaseFileIO.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 22 13:04:57 2021
+
+@author: leand
+"""
+
+import 
+
+class fileIO:
+    
+    def write(filename)
