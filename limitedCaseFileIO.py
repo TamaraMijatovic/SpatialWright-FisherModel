f6ddb6d076a50a1b0617fb79329144a31de93a00
LeanderLutze <L.A.Lutze@student.tudelft.nl>
2021-10-22 15:20:35 +0200

    Update limitedCaseFileIO.py

--- limitedCaseFileIO.py
+++ limitedCaseFileIO.py
@@ -5,8 +5,28 @@ Created on Fri Oct 22 13:04:57 2021
 @author: leand
 """
 
-import 
+import os
 
 class fileIO:
     
-    def write(filename)
+    def create_fresh_file(self, filename):
+        self.set_filename(filename)
+        with open(filename, 'w') as f:
+            f.write("")
+        
+    
+    def set_filename(self, filename):
+        current_dir = os.getcwd()
+        self.filename = current_dir + "/" + filename
+        
+    def append_3D(self, array):
+        return
+    
+    def append_2D(self, array):
+        return
+        
+    def read_3D(self):
+        return
+        
+    def read_2D(self):
+        return
